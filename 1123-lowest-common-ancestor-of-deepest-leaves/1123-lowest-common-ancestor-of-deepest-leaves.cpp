a56e691f7b6991e359bb6682cdd95ee26329fda3
NandiniShrivastav2003 <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-04-03 11:49:28 +0530

    Time: 21 ms (49.59%), Space: 21.6 MB (22.16%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+     bool findPath(TreeNode *root,vector<TreeNode*>&p,int n){
+      if(root == NULL){
+          return false;
+      }
+      p.push_back(root);
+      if(root->val == n){
+          return true;
+      }
+      if(findPath(root->left,p,n) ||findPath(root->right,p,n)){
+          return true;
+      }
+      p.pop_back();
+      return false;
+  }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode*>k1;
+        vector<TreeNode*>k2;
+        if(findPath(root,k1,p->val) == false || findPath(root,k2,q->val) == false){
+            return NULL;
+        }
+        TreeNode* res;       
+        for(int i=0;i<=k1.size()-1 && i<=k2.size()-1;i++)
+        {
+            if(k1[i] == k2[i]){
+               res=k1[i];
+            }
+        }
+      
+        return res;
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        if(!root->left && !root->right){
+            return root;
+        }
+        TreeNode*p=NULL,*t=NULL;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            bool first=true;
+            while(n--){
+                TreeNode* curr=q.front();
+                q.pop();
+                if(first){p=curr;}
+                first=false;
+                t=curr;
+                if(curr->left){
+                    q.push(curr->left);
+            
+                }
+                if(curr->right){
+                    q.push(curr->right);
+                    
+                }
+            }
+        }
+        
+    
+        
+        TreeNode *r=lowestCommonAncestor(root,p,t);
+      return r;
+        
+        
+    }
+};
