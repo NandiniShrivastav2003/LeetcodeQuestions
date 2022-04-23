1e2326c6e9c9a360ef8bdf2a8d4cedc8960ab4e8
NandiniShrivastav2003 <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-04-23 09:16:24 +0530

    Added solution - LeetHub

---
+++ Search insert position of K in a sorted array - GFG/search-insert-position-of-k-in-a-sorted-array.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int searchInsertK(vector<int>Arr, int N, int k)
+    {
+        // code here
+       
+        int lo=0;
+        int high=N-1;  
+        while(lo<=high){
+            int mid=(lo+high)/2;
+             if(k == Arr[mid]){
+                return mid;
+            }
+        
+           
+             else if(k>Arr[mid]){
+              lo=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high+1;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        vector<int>Arr(N);
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int k;
+        cin>>k;
+        Solution obj;
+        cout<<obj.searchInsertK(Arr, N, k)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
