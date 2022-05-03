8f4c88d5486af35bb112603d91873eee90a7226d
NandiniShrivastav2003 <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-05-03 22:38:23 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -8,47 +8,30 @@ class Solution
 	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
-    int findMinVer(int V, vector<bool> &visited,vector<int> &dist){
-	    
-	    int min = INT_MAX;
-	    
-	    int vertex;
-	    
-	    for(int i = 0; i < V; i++){
-	        
-	        if(visited[i] == false && dist[i] < min){
-	            vertex = i;
-	            min = dist[i];
-	        }
-	    }
-	    
-	    return vertex;
-	}
-vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        
-        vector<bool> visited(V,false);
-        vector<int> dist(V,INT_MAX);
-        
+        vector<int> dist(V, INT_MAX);
+        queue<int> q;
+        q.push(S);
         dist[S] = 0;
-        
-        for(int i = 0;i <  (V-1);i++){
-            
-            int u = findMinVer(V,visited,dist);
-            
-            visited[u] = true;
-            
-            for(auto v : adj[u]){
-                 if(!visited[v[0]]   && (dist[u] + v[1]) < dist[v[0]])
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (auto i : adj[node])
+            {
+                int it = i[0];
+                int dis = i[1];
+                if (dist[it] > dist[node] + dis)
                 {
-                    dist[v[0]] = dist[u] + v[1];
+                    dist[it] = dist[node] + dis;
+                    q.push(it);
                 }
             }
         }
         return dist;
     }
 };
-   
 
 
 // { Driver Code Starts.
