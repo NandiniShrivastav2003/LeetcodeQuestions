7d5434785382b0bded67b6512f93b2b9cf2257c4
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-06-16 14:12:50 +0530

    Time: 4 ms (22.32%), Space: 6 MB (55.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
-    void cal(int n,int memo[]){
-    memo[1]=1;
-       memo[2]=2;
-      for(int i=3;i<=n;i++){
-        memo[i]=memo[i-1]+memo[i-2];
-       }
-        
-       return ;
-   }
+    
     int climbStairs(int n) {
         if(n == 1){
             return 1;
@@ -17,7 +9,12 @@ public:
             return 2;
         }
          int memo[n+1];
-    cal(n,memo);
+        memo[1]=1;
+       memo[2]=2;
+      for(int i=3;i<=n;i++){
+        memo[i]=memo[i-1]+memo[i-2];
+       }
+    
         return memo[n];
     }
 };
