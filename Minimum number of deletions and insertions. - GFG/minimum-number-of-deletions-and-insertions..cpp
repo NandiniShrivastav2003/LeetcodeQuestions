7fe82927c59a47cd36aabcd7a5b3bce7c7991773
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-06-19 10:16:48 +0530

    Added solution - LeetHub

---
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+		
+
+	public:
+int LCS(string word1,string word2){
+        int m=word1.size();
+        int n=word2.size();
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+    int minOperations(string word1, string word2) {
+          
+        int lcs=LCS(word1,word2);
+        return word1.size()-lcs+word2.size()-lcs;
+        
+    } 
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+  // } Driver Code Ends
