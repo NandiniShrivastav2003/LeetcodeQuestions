f1dccbcc5ab3e57b98753eb885f85a28cabe59ec
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-06-21 19:43:48 +0530

    Time: 122 ms (55.61%), Space: 10.5 MB (88.17%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+     
+     int dp[coins.size()+1][amount+1];
+        
+        for(int i=1;i<=amount;i++){
+            dp[0][i]=100000;
+        }
+        for(int i=0;i<=coins.size();i++){
+            dp[i][0]=0;
+        }
+         
+        for(int i=1;i<=coins.size();i++){
+            for(int j=1;j<=amount;j++){
+                
+         
+
+              if(j-coins[i-1] >=0){
+                     dp[i][j]=min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+       
+                }
+                else{
+                      dp[i][j]=dp[i-1][j];
+                }
+  }
+        }
+        if(dp[coins.size()][amount] > amount) {
+            return -1;
+        }
+        return dp[coins.size()][amount];
+    }
+};
