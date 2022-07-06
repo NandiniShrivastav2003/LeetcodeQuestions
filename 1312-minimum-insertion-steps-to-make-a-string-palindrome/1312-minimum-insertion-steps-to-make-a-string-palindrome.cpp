489918f7103ea39ef7c80ad4f53c32750195ab6f
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-07-06 16:38:28 +0530

    Time: 30 ms (94.78%), Space: 7.6 MB (57.15%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    // int ans(string s,int out,int i,int j){
+    //     if(i == j){
+    //         return 0;
+    //     }
+    //     if(s[i] == s[j]){
+    //         return ans(s,out,i++,j--);
+    //     }
+    //     return min(ans(s,out+1,i,j-1),ans(s,out+1,i+1,j));
+    // }
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size();
+        int n=text2.size();
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        }
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+    int minInsertions(string s) {
+        //return ans(s,0,0,s.size()-1);
+      string t="";
+         for (int i = s.size() - 1; i >= 0; i--){
+             t+=s[i];
+         }
+        return s.size()-longestCommonSubsequence(s,t);
+    }
+};
