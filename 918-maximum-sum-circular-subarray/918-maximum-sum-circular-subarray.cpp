539c29408c1fa31b3155aabed9bead608a67eb77
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-09-03 19:52:15 +0530

    Time: 189 ms (5.06%), Space: 39.9 MB (32.53%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    int kadane(vector<int>& A){
-        int currentSum = A[0];
-        int maxSum = A[0];
-        for(int i=1;i<A.size();i++){
-            if(currentSum < 0)
-                currentSum = 0;
-            currentSum = A[i] + currentSum;
-            maxSum =max(maxSum,currentSum);
-        }
-        return maxSum;
-    }
-//     int kadane(vector<int>&nums){
+    // int kadane(vector<int>& A){
+    //     int currentSum = A[0];
+    //     int maxSum = A[0];
+    //     for(int i=1;i<A.size();i++){
+    //         if(currentSum < 0)
+    //             currentSum = 0;
+    //         currentSum = A[i] + currentSum;
+    //         maxSum =max(maxSum,currentSum);
+    //     }
+    //     return maxSum;
+    // }
+    int kadane(vector<int>&nums){
 
-//         int ans=0;
-//         int res=nums[0];
-//         for(int i=0;i<nums.size();i++){
-//             ans+=nums[i];
-           
-//             if(ans<0){
-//                 ans=0;
-//             }
-//               res=max(res,ans);
+        int ans=nums[0];
+        int res=nums[0];
+        for(int i=1;i<nums.size();i++){
             
-//         }
-//         return res;
-//     }
+           
+            if(ans<0){
+                ans=0;
+            }
+            ans+=nums[i];
+              res=max(res,ans);
+            
+        }
+        return res;
+    }
     int maxSubarraySumCircular(vector<int>& nums) {
         int ans=0;
         int sum2=kadane(nums);
