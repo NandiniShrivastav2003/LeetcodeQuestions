5804e7ae1cf7f5849386a2055dd988e0ad609472
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-11-11 00:11:26 +0530

    Time: 358 ms (5.03%), Space: 109.9 MB (5.10%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+     int findceil(int tail[],int l,int r,int x){
+        while(r>l){
+            int m=l+(r-l)/2;
+            if(tail[m] >= x){
+                r=m;
+            }
+            else{
+                
+                l=m+1;
+            }
+            
+        }
+        return r;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int tail[nums.size()];
+        int len=1;
+        tail[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] > tail[len-1]){
+                tail[len]=nums[i];
+                len++;
+            }
+            else{
+                int c=findceil(tail,0,len-1,nums[i]);
+                tail[c]=nums[i];
+            }
+        }
+        return len;
+    }
+    bool increasingTriplet(vector<int>& nums) {
+        int ans=lengthOfLIS(nums);
+        if(ans >= 3){
+            return true;
+        }
+        return false;
+    }
+};
