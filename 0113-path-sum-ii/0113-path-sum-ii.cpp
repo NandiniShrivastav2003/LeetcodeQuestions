4177d15a6d38674d993bac1dd4433d843cebfe30
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-12-19 21:44:41 +0530

    Time: 19 ms (59.26%), Space: 19.8 MB (80.12%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>>res;
+    vector<int>temp;
+    void allPaths(TreeNode* root, int targetSum,int sum)
+    {
+        if(root == NULL){
+            return ;
+        }
+        sum+=root->val;
+        temp.push_back(root->val);
+      
+        if(sum == targetSum && root->left == NULL && root->right ==NULL){
+            res.push_back(temp);
+            
+        }
+        else{
+        allPaths(root->left,targetSum,sum);
+        allPaths(root->right,targetSum,sum);
+        }
+        temp.pop_back();
+        
+      
+        
+    }    
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        allPaths(root,targetSum,0);
+        return res;
+    }
+};
