a8d54467806455e675ac176546696f8a22e13eea
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2022-12-20 11:45:43 +0530

    Time: 29 ms (73.56%), Space: 18 MB (66.12%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int left=0;int right=nums.size();
-        int mid;
-        while(left<right){
-            mid=(left+right)/2;
-            if(nums[mid] > mid){
-                right=mid;
-            }
-            else{
-                left=mid+1;
-            }
+         int i=0;
+        int n = nums.size();
+        while(i<n)
+        {
+			int correctIdx = nums[i]; //where this element should be in sorted array
+            if(correctIdx<n && correctIdx != i) //if not already at correct position and correct position < n
+            {
+                swap(nums[i],nums[correctIdx]); //put current element at correct position
+            } 
+			else
+				i++; // move to next index
         }
-        return left;
+        for(int i=0;i<n; i++)
+            if(nums[i]!=i)
+                return i;
+        return n;
     }
 };
