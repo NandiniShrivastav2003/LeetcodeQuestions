a65524378aafdc3e2480c11c46a694ee3d2a499d
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-01-08 21:56:14 +0530

    Time: 142 ms (79.57%), Space: 42.3 MB (70.09%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,37 +1,78 @@
+class DisjointSet{
+public:
+    vector<int>parent,usize;
+    DisjointSet(int n){
+             usize.resize(n+1);
+        parent.resize(n+1);
+        
+        for(int i = 0 ; i<=n;i++)
+        {
+            usize[i] =1 ;
+            parent[i] =i;
+        }
+   
+    }
+    int findUPar(int n){
+         if(parent[n] == n)
+        {
+            return n;
+        }
+        else
+        {
+            return parent[n] = findUPar(parent[n]);
+        }
+    }
+    void UnionBySize(int u , int v)
+    {
+        int ultp_u = findUPar(u);
+        int ultp_v = findUPar(v);
+        
+        if(ultp_u == ultp_v)
+            return;
+        else if(usize[ultp_u] > usize[ultp_v])
+        {
+            parent[ultp_v] = ultp_u;
+            usize[ultp_u] += usize[ultp_v];
+        }
+        else
+        {
+            parent[ultp_u] = ultp_v;
+            usize[ultp_v] += usize[ultp_u];
+        }
+    }
+};
 class Solution {
 public:
-
-     void dfs(int v,vector<vector<int>>&adj,vector<bool>&visited){
-     
-       visited[v]=true;
-       for(int x:adj[v]){
-            if(visited[x] == false){
-            
-            
-                dfs(x,adj,visited);
-                
+    int makeConnected(int n, vector<vector<int>>& edges) {
+/*using disjoint set(union find)
+Note for me:
+1) DSU extra edge of same component ko ignore karta hai
+2) DSU minimum edge ka connected graph hota hai ( for visualization )
+3) component of graph ki bat ho rahi think of DSU once
+*/
+        DisjointSet ds (n);
+        int cntExtras=0;
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            if(ds.findUPar(u) == ds.findUPar(v)){
+                cntExtras++;
             }
-  }
-  }
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size() < n-1 ){
-            return -1;
+            else{
+                ds.UnionBySize(u,v);
+            }
         }
-        vector<vector<int>>adj(n);
-        for(auto v:connections){
-           adj[v[0]].push_back(v[1]);
-            adj[v[1]].push_back( v[0]);
-        }
-        int ans=0;
-vector<bool>visited(n,false);
+        int cntC=0;
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-            dfs(i,adj,visited);
-            ans++;
+            if(ds.findUPar(i) == i){
+                cntC++;
             }
         }
-         return ans-1;
-       
-  
+        int ans=cntC-1;
+        if(cntExtras >= ans){
+            return ans;
+        }
+        return -1;
+        
     }
 };
