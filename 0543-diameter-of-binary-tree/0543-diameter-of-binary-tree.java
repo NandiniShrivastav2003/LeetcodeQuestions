0d3e4fa14396514bbc4e25d359f55a01d1803aa0
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-01-30 13:28:58 +0530

    Time: 1 ms (47.88%), Space: 42 MB (74.93%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,36 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
- //   TC:O(n2)
-    public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null){
-            return 0;
-        }
-        int ld=diameterOfBinaryTree(root.left);
-        int rd=diameterOfBinaryTree(root.right);
-        int sd=ht(root.left)+ht(root.right)+2;
-        return Math.max(ld,Math.max(sd,rd));
+    //whenever we want O(N) Time complexity we will maintain a pair of height and diameter.to visit every node only once.
+    public class DiaPair{
+        int ht=-1;
+        int d=0;
     }
-    public int ht(TreeNode root){
+    public int diameterOfBinaryTree(TreeNode root) {
+        return diameter(root).d;
+    }
+    public DiaPair diameter(TreeNode root){
         if(root == null){
-            return -1;
+            return new DiaPair(); 
         }
-        int lh=ht(root.left);
-        int rh=ht(root.right);
-        return Math.max(lh,rh)+1;
-        
+        DiaPair ldp=diameter(root.left);
+        DiaPair rdp=diameter(root.right);
+        DiaPair sdp=new DiaPair();
+        int sd=ldp.ht+rdp.ht+2;
+        sdp.d=Math.max(ldp.d,Math.max(rdp.d,sd));
+        sdp.ht=Math.max(ldp.ht,rdp.ht)+1;
+        return sdp;
     }
 }
