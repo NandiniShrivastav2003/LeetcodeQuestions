f2d11e03930768fbccf8e93792cdf81de5efe88a
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-02-03 23:37:19 +0530

    Time: 26 ms (28.98%), Space: 14.5 MB (33.00%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
-    int getcount(vector<int>&dp , int zero , int one , int n)
-    {
-        if(n == 0) return 1;
-        if(n < 0) return 0;
-        
-        if(dp[n] != -1) return dp[n];
-        
-        return dp[n] = (getcount(dp,zero,one,n-zero) % mod + getcount(dp,zero,one,n-one) % mod) % mod;
+    int mod=1e9+7;
+    int countStrings(vector<int>&dp,int zero,int one,int n){
+        if(n == 0){
+            return 1 ;
+        }
+        if(n<0){
+            return 0;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+         dp[n]=(countStrings(dp,zero,one,n-zero) % mod+
+        countStrings(dp,zero,one,n-one) %mod)%mod;
+        return dp[n];
     }
-    
-    int countGoodStrings(int low, int high, int zero, int one)
-    {
-        int ans = 0;
+    int countGoodStrings(int low, int high, int zero, int one) {
         vector<int>dp(high+1,-1);
-        
-        for(int i = low ; i <= high ; i++)
-        {
-            ans = (ans + getcount(dp , zero , one , i) % mod) % mod;
+        int ans=0;
+        for(int i=low;i<=high;i++){
+           ans= (ans+countStrings(dp,zero,one,i)%mod)%mod;
         }
         return ans;
     }
