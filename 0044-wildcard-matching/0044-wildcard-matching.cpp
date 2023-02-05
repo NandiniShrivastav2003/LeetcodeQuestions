fa101f12910391b05044eb71cdd9f46d64bc0ab4
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-02-05 19:16:26 +0530

    Time: 89 ms (45.97%), Space: 27.9 MB (13.50%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m=s.size();
+        int n=p.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+             if(p[i-1] == '*'){
+                dp[0][i]=dp[0][i-1]&& 1;
+            }
+            else{
+                dp[0][i]=0;
+            }
+        }   
+        for(int i=1;i<=m;i++){
+            if(s[i-1] == '*'){
+                dp[i][0]=1;
+            }
+            else{
+                dp[i][0]=0;
+            }
+        }
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == p[j-1] || s[i-1] == '?' || p[j-1] == '?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(s[i-1] == '*' || p[j-1] == '*'){
+                    dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                }
+                else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        // for(int i=1;i<=m;i++){
+        //     for(int j=1;j<=n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return dp[m][n];
+        
+    }
+};
