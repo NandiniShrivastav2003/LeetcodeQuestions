af34d95f11f56d6ae45bbea903ac8572df910eb4
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-02-09 20:44:41 +0530

    Time: 4 ms (43.77%), Space: 7.8 MB (11.74%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n+1);
-        dp[n] = 1;
-        for(int i=n-1;i>=0;i--) {
-            if(s[i]=='0') dp[i]=0;
-            else {
-                dp[i] = dp[i+1];
-                if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) dp[i]+=dp[i+2];
-            }
+    vector<string>ans;
+    int res=0;
+      unordered_map<string,int>mp;
+    int encoding(string s,int i,string ans,vector<int>&dp){
+        if(i>s.size()){
+            return 0;
         }
-        return s.empty()? 0 : dp[0]; 
+        if(s[i] == '0'){
+            return 0;
+        }
+        if(i == s.size()){
+            
+            return 1;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+            dp[i]=encoding(s,i+1,ans,dp);
+        if(s.substr(i,2) >= "0" && s.substr(i,2) <= "26"){
+            
+        
+        dp[i]+=encoding(s,i+2,ans,dp);
+        }
+     return dp[i];
+    }
+    int numDecodings(string s) {
+        int ans=0;
+      
+        char ch='A';
+        vector<int>dp(101,-1);
+        encoding(s,0,"",dp);
+        
+     return dp[0] == -1 ?0:dp[0];   
     }
 };
