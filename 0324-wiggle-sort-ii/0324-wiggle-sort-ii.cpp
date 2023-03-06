6555796f5819a59f1f7fb3849815bf1e7c17596d
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-03-06 11:17:55 +0530

    Time: 23 ms (64.43%), Space: 17.8 MB (54.25%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -8,7 +8,10 @@ public:
         nums[i]=temp[j];
              j--;
        }
-       
+       //intution-we sort the array and fill half larger elements at odd index since
+        //we want odd index elements to be larger than even indexed elements then we fill even indexed elements with value from arr[mid-1 ] to 0.
+        
+        //note -- we can't fill elements in even indexed from 0 to arr[mid-1] as it will result in failed test case in -[1,3,2,2,3,1] -> [1,3,1,3,2,2] which is false but acc to above method:[2,3,1,3,1,2] (arr[mid-1] to 0);
         for(int i=0;i<nums.size();i+=2){
             nums[i]=temp[j];
             j--;
