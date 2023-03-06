43690108cfd9738cc7baefc5e5f53daca3dd7cbd
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-03-06 11:13:46 +0530

    Time: 19 ms (85.38%), Space: 17.9 MB (20.28%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+         vector<int>temp=nums;
+        sort(temp.begin(),temp.end());
+        int j=temp.size()-1;
+         for(int i=1;i<nums.size();i+=2){
+        nums[i]=temp[j];
+             j--;
+       }
+       
+        for(int i=0;i<nums.size();i+=2){
+            nums[i]=temp[j];
+            j--;
+        }
+        
+        return ;
+    }
+};
