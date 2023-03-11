065b5053ee1df1901818fd34e9e38aff363c3c87
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-03-11 19:12:13 +0530

    Time: 32 ms (20.68%), Space: 10.8 MB (29.52%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   static bool cmp(pair<int,int>&a,pair<int,int>&b){
+   static bool cmp(pair<int,int>a,pair<int,int>b){
         if(a.first == b.first){
             return a.second  > b.second; 
         }
