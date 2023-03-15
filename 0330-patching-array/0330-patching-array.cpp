a999eb611eb857c545fb5e4d27515ad0ba897cfd
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-03-15 09:55:38 +0530

    Time: 8 ms (55.72%), Space: 11.3 MB (55.72%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long  missing=1;
+        int count=0;
+        int i=0;
+        while(missing <= n){
+             if(i<nums.size() && missing >=nums[i]){
+                 missing+=nums[i];
+                 i++;
+             }
+            else{
+                count++;
+                missing+=missing;
+            }
+        }
+        return count;
+    }
+};
