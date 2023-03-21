44451fcd9706f36fdaf484b8ecbfdc9cfe3233a9
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-03-21 14:29:50 +0530

    Time: 544 ms (86.53%), Space: 187.5 MB (34.07%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    void dfs(int currNode, int& ans, vector<vector<int>>& neighbors, vector<bool>& seen) {
-        // Mark 'currNode' as visited and increment 'ans' by 1.
-        ans++;
-        seen[currNode] = true;
-        
-        // Go for unvisited neighbors of 'currNode'.
-        for (auto nextNode : neighbors[currNode]) {
-            if (!seen[nextNode]) {
-                dfs(nextNode, ans, neighbors, seen);
-            }
+    int c=0;
+    void dfs( vector<vector<int>>&adj,vector<int>&vis,int u){
+      vis[u]=1;
+        c++;
+    for(int v:adj[u]){
+        if(vis[v] == 0){
+        dfs(adj,vis,v);
         }
     }
-    
-    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
-        //Store all edges according to nodes in 'neighbors'.
-        vector<vector<int>> neighbors(n);
-        for (auto edge : edges) {
-            int nodeA = edge[0], nodeB = edge[1];
-            neighbors[nodeA].push_back(nodeB);
-            neighbors[nodeB].push_back(nodeA);
+    }
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& res) {
+        vector<int>vis(n,0);
+        for(int i:res){
+            vis[i]=1;
+            
         }
-        
-        // Mark the nodes in 'restricted' as visited.
-        vector<bool> seen(n, false);
-        for (auto node : restricted) {
-             seen[node] = true;
+        vector<vector<int>>adj(n);
+        for(auto i:edges){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
         }
-        
-        int ans = 0;
-        dfs(0, ans, neighbors, seen);
-        return ans;
+        dfs(adj,vis,0);
+        return c;
     }
 };
