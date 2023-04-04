5c4a3da6db45b3284a3a2f6b944faa4e0186212d
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-04-04 13:41:27 +0530

    Time: 16 ms (6.95%), Space: 22.9 MB (9.20%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,42 +10,22 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
-        ListNode *res=new ListNode(-1);
-		ListNode *newLL=res;
-		// head2=head2->next;
-		// head1=head1->next;
-		while(head1 != NULL && head2 != NULL){
-			if(head1->val == head2->val){
-res->next=new ListNode(head1->val);
-res=res->next;
-                res->next=new ListNode(head2->val);
-res=res->next;
-head1=head1->next;
-head2=head2->next;
-			}
-			else if(head1->val < head2->val){
-res->next=new ListNode(head1->val);
-res=res->next;
-head1=head1->next;
-
-			}
-			else{
-res->next=new ListNode(head2->val);
-res=res->next;
-head2=head2->next;
-			}
-		}
-		while(head1 != NULL){
-			res->next=new ListNode(head1->val);
-res=res->next;
-head1=head1->next;
-		}
-			while(head2 != NULL){
-			res->next=new ListNode(head2->val);
-res=res->next;
-head2=head2->next;
-		}
-        return newLL->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+            return list2;
+        }
+        if(list2 == NULL){
+            return list1;
+        }
+        ListNode *res;
+        if(list1->val <= list2->val){
+            res=new ListNode(list1->val);
+            res->next=mergeTwoLists(list1->next,list2);
+        }
+         else if(list1->val > list2->val){
+           res=new ListNode(list2->val);
+            res->next=mergeTwoLists(list1,list2->next);
+        }
+        return res;
     }
 };
