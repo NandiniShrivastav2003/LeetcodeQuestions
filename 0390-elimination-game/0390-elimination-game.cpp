231249aca2ae0f8975f055a55ae3895a31ad9cca
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-05-07 18:23:27 +0530

    Time: 8 ms (34.58%), Space: 6 MB (56.78%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int lastRemaining(int n) {
+        //we will use math and some observations like if there is right turn then our start pos willnot change as value will be cut from right side which does not cut our curr pos 
+        //ex- 2 4 6 8 here if our curr pos=2 and right = true then 2 will remain same 
+        //but 2 4 6 8 10 then 2 will change.hence if(n% 2 ==  1){
+       // then change start.
+    //}
         int start=1;
         int steps=1;
         bool left=true;
