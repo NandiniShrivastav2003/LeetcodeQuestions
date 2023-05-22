1c3638ca79d3f7395a0cf87425764ff137d6c916
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-05-23 00:16:08 +0530

    Time: 56 ms (93.54%), Space: 64.1 MB (5.73%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,18 @@
+class Solution {
+public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int[] spells0 = spells.clone();
+        Arrays.sort(potions);
+        Arrays.sort(spells);
+        HashMap<Integer, Integer> count = new HashMap<>();
+        int n = spells.length, m = potions.length, j = m - 1, res[] = new int[n];
+        for (int i = 0; i < n; ++i) {
+            while (j >= 0 && 1L * spells[i] * potions[j] >= success)
+                j--;
+            count.put(spells[i], m - j - 1);
+        }
+        for (int i = 0; i < n; ++i) {
+            res[i] = count.get(spells0[i]);
+        }
+        return res;
+    }
+}
