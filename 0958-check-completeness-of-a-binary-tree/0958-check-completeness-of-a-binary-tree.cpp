61ce7c312ef1f452567ced4a7c59ec68f05ca2ba
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-05-26 23:39:33 +0530

    Time: 7 ms (39.04%), Space: 10.3 MB (76.86%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -11,49 +11,22 @@
  */
 class Solution {
 public:
-//     int ans=-1;
-//     int height(TreeNode *root){
-//         if(root == NULL){
-//             return 0;
-//         }
-//        int a= height(root->left);
-//         int b=height(root->right);
-     
-//      return max(a,b)+1;
-      
-//     }
+
+
     bool isCompleteTree(TreeNode* root) {
-        // int h=height(root);
-        //cout<<h<<endl;
+       
     queue<TreeNode*>q;
         
         q.push(root);
         bool f=false;
       
         while(!q.empty()){
-            
-//             int lh=pow(2,l);
-//             int size=q.size();
-//              if(lh != size && l != h-1){
-                
-//              return false;
-//          }
-            //while(size>0){
-                //size=size-1;
                TreeNode *temp=q.front();
             q.pop();
             if(temp == NULL){
                 f=true;
             }
-//                 if(temp->left == NULL && temp->right != NULL){
-//                     return false;
-//                 }
-                
-//                 if(temp->right == NULL && size > 0 && l != h-1) {
-//                     cout<<size<<" "<<temp->val<<" "<<l<<endl;
-//                     return false;
-//                 }
-               else{
+else{
                    if(f){
                        return false;
                    }
