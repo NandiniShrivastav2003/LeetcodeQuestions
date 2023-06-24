7373c5de9d2edc2b05ff400440919a260ad286ea
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-06-24 18:48:59 +0530

    Time: 632 ms (38.54%), Space: 204.1 MB (15.12%) - LeetHub

--- 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        //we will maintain count of frequencies of string s till index i.
         vector<vector<int>>prefix;
         vector<int>temp(26,0);
         vector<bool>ans;
-        prefix.push_back(temp);
+        //for conditions like [0,4] we will push initial 0= value as temp[0,...0]
+         prefix.push_back(temp);
         for(int i=0;i<s.size();i++){
             temp[s[i]-'a']++;
             prefix.push_back(temp);
@@ -14,14 +16,18 @@ public:
             int r=q[1];
             int k=q[2];
              int odd=0;
+            
              vector<int>count=prefix[r+1];
           for(int i=0;i<26;i++){
+              //subtract freq[r+1]-freq[l]
                 count[i]-=prefix[l][i];
+              
                 odd+=(count[i]%2);
             }
           
-       odd-=(odd%2);
-      ans.push_back((odd<=k*2)?true:false);
+       odd-=(odd%2); // if odd is odd like 5 then we will subtract 1 as abcba //atmost one can be odd.
+            
+      ans.push_back((odd<=k*2)?true:false); //  we can change odds/2 elements to match other odds/2 elements so odd/2<=k or odd<=2*k
         }
         return ans;
         
