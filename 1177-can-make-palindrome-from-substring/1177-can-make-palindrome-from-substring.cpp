1f24b34f3507b36a36a4f9648f9e4a4de6545fd9
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-06-24 18:45:00 +0530

    Time: 661 ms (31.71%), Space: 204.2 MB (15.12%) - LeetHub

---
+++ 1177-can-make-palindrome-from-substring/1177-can-make-palindrome-from-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
+        vector<vector<int>>prefix;
+        vector<int>temp(26,0);
+        vector<bool>ans;
+        prefix.push_back(temp);
+        for(int i=0;i<s.size();i++){
+            temp[s[i]-'a']++;
+            prefix.push_back(temp);
+        }
+        for(auto q:queries){
+            int l=q[0];
+            int r=q[1];
+            int k=q[2];
+             int sum=0;
+             int odd=0;
+             vector<int>count;
+           count=prefix[r+1];
+          for(int i=0;i<26;i++){
+                count[i]-=prefix[l][i];
+                sum+=count[i];
+                odd+=(count[i]%2);
+            }
+          
+       odd-=(odd%2);
+      ans.push_back((odd<=k*2)?true:false);
+        }
+        return ans;
+        
+    }
+};
