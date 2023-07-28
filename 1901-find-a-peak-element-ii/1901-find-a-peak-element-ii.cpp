c30a4a73728b7aa4df95bda16adeaff71d6d5788
Nandini Shrivastav <86567455+NandiniShrivastav2003@users.noreply.github.com>
2023-07-28 15:58:54 +0530

    Time: 151 ms (99.55%), Space: 46.4 MB (40.30%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -3,35 +3,16 @@ public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-       int i=m-1;
-        int j=n-1;
-        int t1,t2;
-        bool flag=false;
-        while(i>=0 && j>=0){
-            flag=false;
-            if(i+1 >=0 && i+1 <m && mat[i+1][j] > mat[i][j]){
-                t1=i+1;t2=j;
-                flag=true;
-            } 
-            
-             if(i-1 >=0 && i-1 <m && mat[i-1][j] > mat[i][j]){
-                t1=i-1;t2=j;
-                    flag=true;
-            } 
-             if(j+1 >=0 && j+1 <n && mat[i][j+1] > mat[i][j]){
-                t1=i;t2=j+1;
-                    flag=true;
-            } 
-             if(j-1 >=0 && j-1 <n && mat[i][j-1] > mat[i][j]){
-                t1=i;t2=j-1;
-                    flag=true;
-            } 
-            if(!flag){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int a=(i+1 >=0 && i+1 <m)?(mat[i][j] > mat[i+1][j]):1;
+                  int b=(i-1 >=0 && i-1 <m)?(mat[i][j] > mat[i-1][j]):1;
+                  int c=(j+1 >=0 && j+1 <n)?(mat[i][j] > mat[i][j+1]):1;
+                  int d=(j-1 >=0 && j-1 <n)?(mat[i][j] > mat[i][j-1]):1;
+            if(a&&b&&c&&d){
                 return {i,j};
             }
-            i=t1;
-            j=t2;
-            
+            }
         }
         return {-1,-1};
     }
